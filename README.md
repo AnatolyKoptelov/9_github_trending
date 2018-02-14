2d343e99f775ebee22e295ecc2a20186b4c04469
AnatolyKoptelov <7497490@gmail.com>
2018-02-14 17:48:06 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,7 +1,42 @@
 # Github Trends
 
-[TODO. There will be project description]
+Application for getting of most Trending Repositories.
+By default it printing 20 most Trending Repositories, created in less a week.
+You can change period and quantity of interesting repositories:
+ - use optional parameter **-q** for change quantity
+ - use optional parameter **-d** for change period
+ 
+Application is printing a list of most Trending Repositories:
+ - URL
+ - Creating date
+ - Stars quantity
+ - Opened issue quantity
+ 
+# Quickstart
+Run with command line:
+ ```
+$ python github_trending.py -d 5 -q 5
+HTML URL of the Trending Repository                             Was created     Stars   Opened issues
+https://github.com/vektah/gqlgen                                2018-02-11        588               2
+https://github.com/truedread/netflix-1080p                      2018-02-11        391               0
+https://github.com/raphael-ernaelsten/Aura                      2018-02-12        340               6
+https://github.com/Eplox/TCP-Starvation                         2018-02-12        277               3
+https://github.com/mitchellh/go-server-timing                   2018-02-12        233               1
+```
+or get help:
+```
+$ python github_trending.py -h
+usage: github_trending.py [-h] [-q QUANTITY] [-d DAYS]
 
+GitHub Trending
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -q QUANTITY, --quantity QUANTITY
+                        How much repositories to displayUsing 20 by default
+  -d DAYS, --days DAYS  How long ago repositories were createdUsing 7 days by
+                        default
+```
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,78 @@
+import requests
+import argparse
+from datetime import datetime, timedelta
 
 
-def get_trending_repositories(top_size):
-    pass
+def get_args():
+    default_quantity = 20
+    default_period = 7
+    parser = argparse.ArgumentParser(
+        description='GitHub Trending'
+    )
+    parser.add_argument(
+        '-q',
+        '--quantity',
+        type=int,
+        default=default_quantity,
+        help='{}{}'.format(
+            'How much repositories to display',
+            'Using 20 by default',
+        )
+    )
+    parser.add_argument(
+        '-d',
+        '--days',
+        type=int,
+        default=default_period,
+        help='{}{}'.format(
+            'How long ago repositories were created',
+            'Using 7 days by default',
+        )
+    )
+    return parser.parse_args()
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
+def get_trending_repositories(top_size, created_after):
+    url = 'https://api.github.com/search/repositories'
+    get_params = {
+        'q': '{}{}'.format(
+            'created:>=',
+            created_after,
+        ),
+        'sort': 'stars'
+    }
+    response = requests.get(url, params=get_params)
+    if response.ok:
+        return response.json()['items'][:top_size]
+
+
+def repository_for_print(repository_data):
+    length_of_date_string = len('YYYY-MM-DD')
+    return '{:60}\t{:12}\t{:5}\t{:13}'.format(
+        repository_data['html_url'],
+        repository_data['created_at'][:length_of_date_string],
+        repository_data['stargazers_count'],
+        repository_data['open_issues_count'],
+    )
+
 
 if __name__ == '__main__':
-    pass
+    args = get_args()
+    try:
+        trending_repositories = get_trending_repositories(
+            args.quantity,
+            datetime.strftime(
+                datetime.now() - timedelta(days=args.days),
+                '%Y-%m-%d',
+            )
+        )
+        print('{:60}\t{:12}\t{:5}\t{:13}'.format(
+            'HTML URL of the Trending Repository',
+            'Was created',
+            'Stars',
+            'Opened issues',
+        ))
+        for repository in trending_repositories:
+            print(repository_for_print(repository))
+    except requests.exceptions.RequestException as error:
+        print(error)
