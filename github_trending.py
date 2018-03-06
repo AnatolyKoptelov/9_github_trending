4296a6a248169cafb47687f95b62c310ae8249d2
AnatolyKoptelov <7497490@gmail.com>
2018-03-06 15:40:58 +0300

    Add get_open_issues_amount function

--- github_trending.py
+++ github_trending.py
@@ -4,8 +4,6 @@ from datetime import datetime, timedelta
 
 
 def get_args():
-    default_quantity = 20
-    default_period = 7
     parser = argparse.ArgumentParser(
         description='GitHub Trending'
     )
@@ -13,7 +11,7 @@ def get_args():
         '-q',
         '--quantity',
         type=int,
-        default=default_quantity,
+        default=20,
         help='{}{}'.format(
             'How much repositories to display',
             'Using 20 by default',
@@ -23,7 +21,7 @@ def get_args():
         '-d',
         '--days',
         type=int,
-        default=default_period,
+        default=7,
         help='{}{}'.format(
             'How long ago repositories were created',
             'Using 7 days by default',
@@ -46,17 +44,31 @@ def get_trending_repositories(top_size, created_after):
         return response.json()['items'][:top_size]
 
 
-def repository_for_print(repository_data):
-    length_of_date_string = len('YYYY-MM-DD')
-    return '{:60}\t{:12}\t{:5}\t{:13}'.format(
+def get_open_issues_amount(repo_owner, repo_name):
+    url = '{}{}/{}/{}'.format(
+        'https://api.github.com/repos/',
+        repo_owner,
+        repo_name,
+        'issues',
+    )
+    response = requests.get(url)
+    if response.ok:
+        return len(response.json())
+
+
+def format_for_print(repository_data, amount):
+    length_of_date_string = 10
+    return '{:70}\t{:12}\t{:5}\t{:13}'.format(
         repository_data['html_url'],
         repository_data['created_at'][:length_of_date_string],
         repository_data['stargazers_count'],
-        repository_data['open_issues_count'],
+        amount,
     )
 
 
 if __name__ == '__main__':
+    trending_repositories = None
+    open_issues_amount = []
     args = get_args()
     try:
         trending_repositories = get_trending_repositories(
@@ -66,13 +78,22 @@ if __name__ == '__main__':
                 '%Y-%m-%d',
             )
         )
-        print('{:60}\t{:12}\t{:5}\t{:13}'.format(
+        open_issues_amount = trending_repositories and [
+            get_open_issues_amount(
+                repository['owner']['login'],
+                repository['name'],
+            ) for repository in trending_repositories
+        ] or None
+    except requests.exceptions.RequestException as error:
+        print(error)
+    if trending_repositories and open_issues_amount:
+        print('{:70}\t{:12}\t{:5}\t{:13}'.format(
             'HTML URL of the Trending Repository',
             'Was created',
             'Stars',
             'Opened issues',
         ))
-        for repository in trending_repositories:
-            print(repository_for_print(repository))
-    except requests.exceptions.RequestException as error:
-        print(error)
+        for position, repository in enumerate(trending_repositories):
+            print(format_for_print(repository, open_issues_amount[position]))
+    else:
+        print('Cannot read repositories data. Try again later!')
